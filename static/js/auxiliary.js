78ec3f56c69eb046067c6081fb44b0ca4e908bde
Yang Kun <330661125@qq.com>
2021-02-02 16:41:03 +0800

    静态文件

---
+++ static/js/auxiliary.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Yang Kun
+ * @Descripttion: 
+ * @Date: 2020-10-21 11:36:17
+ * @LastEditors: Yang Kun
+ * @LastEditTime: 2020-10-22 10:56:35
+ */
+$(function () {
+    $(".article_info .content table").addClass("layui-table");
+    $(".article_info .content table").addClass("layui-table");
+    $(".article_info .content pre").addClass("layui-code");
+    $(".article_info .content blockquote").addClass("layui-elem-quote").addClass("layui-quote-nm");
+
+});
+
+layui.use('layer', function () {
+    var layer = layui.layer;
+}); 
